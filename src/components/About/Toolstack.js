fca382381d776654e7fdd9928a67b392f7cea3a8
ali1373107 <alidashti1994a@gmail.com>
2024-08-12 23:41:43 +0100

    component created

---
+++ src/components/About/Toolstack.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { Col, Row } from "react-bootstrap";
+import {
+  SiVisualstudiocode,
+  SiPostman,
+  SiSlack,
+  SiVercel,
+  SiAndroidstudio,
+  SiSpring,
+  SiDocker,
+  SiMicrosoft,
+} from "react-icons/si";
+
+function Toolstack() {
+  return (
+    <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiVisualstudiocode />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiPostman />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiSlack />
+      </Col>
+
+      <Col xs={4} md={2} className="tech-icons">
+        <SiAndroidstudio />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiSpring />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiDocker />
+      </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <SiMicrosoft /> {/* Microsoft icon */}
+      </Col>
+    </Row>
+  );
+}
+
+export default Toolstack;
