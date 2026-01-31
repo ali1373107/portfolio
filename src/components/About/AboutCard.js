0b11dd37109e23cbb7f592610e96ec3bab08296b
Ali Dashti <alidashti1994a@gmail.com>
2026-01-31 18:59:54 +0000

    Name Updated

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -7,7 +7,7 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Ali Dashti</span>. My
+            Hi Everyone, I am <span className="purple">Allan Dashti</span>. My
             passion for problem-solving and creating impactful solutions drives
             me to constantly explore new technologies and frameworks. I'm
             currently pursuing a Master's in AI for Engineering at the
